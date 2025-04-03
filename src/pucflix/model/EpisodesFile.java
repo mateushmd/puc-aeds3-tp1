ec1c0edf9aa388a13446f5b3eab48e1dffbc5941
guisilva <guilhermesoaressilvadev@gmail.com>
2025-04-03 11:26:39 -0300

    Update entity/Episode.java and model/EpisodesFile.java

--- src/pucflix/model/EpisodesFile.java
+++ src/pucflix/model/EpisodesFile.java
@@ -1,4 +1,4 @@
-package pucflix.model;
+package model;
 
 public class EpisodesFile extends File 
 {
@@ -7,24 +7,28 @@ public class EpisodesFile extends File
         name = "Episódios";
     }
 
+    // Create Episode
     @Override
     public void create() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'create'");
     }
 
+    // Read Episode
     @Override
     public void read() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'read'");
     }
 
+    // Update Episode
     @Override
     public void update() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'update'");
     }
 
+    // Delete Episode
     @Override
     public void delete() {
         // TODO Auto-generated method stub
