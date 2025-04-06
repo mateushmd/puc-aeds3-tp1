7de6baa0b56e587564e8391788bda4c21826ba57
guisilva <guilhermesoaressilvadev@gmail.com>
2025-04-06 11:06:43 -0300

    🚧 feat: EpisodeFile

--- src/pucflix/model/EpisodeFile.java
+++ src/pucflix/model/EpisodeFile.java
@@ -1,51 +1,87 @@
-package model;
+package pucflix.model;
 
-import aeds3.*;
+import pucflix.entity.Episode;
+import pucflix.aeds3.Arquivo;
+import pucflix.aeds3.HashExtensivel;
+import pucflix.aeds3.ArvoreBMais;
 
-public class EpisodeFile extends aeds3.File<Episode> 
+public class EpisodeFile extends Arquivo<Episode> 
 {
+    HashExtensivel<EpisodeNameIdPair> nameIndex;    
 
-    File<Episode> fileEpisode;
-    ExtensiveHash<ParNameId> indirectIndexName;
-
-    public EpisodeFile()
+    public EpisodeFile() throws Exception
     {
-        name = "Episódios";
+        super("episodes", Episode.class.getConstructor());
+        nameIndex = new HashExtensivel<>(
+            EpisodeNameIdPair.class.getConstructor(),
+            4,
+            "./dados/" + nomeEntidade + "/nameIndex.d.db",
+            "./dados/" + nomeEntidade + "/nameIndex.c.db"
+        );
     }
 
-    // Create Episode
     @Override
-    public void create(Episode e) throws Exception{
-        int id = super.create(e);
-        indirectIndexName.create(new parNameId(e.getName(), id))
+    public void create(Episode episode) throws Exception 
+    {
+        int id = super.create(episode);
+        nameIndex.create(new EpisodeNameIdPair(episode.getName(), id));
+        return id;
     }
 
-    // Read Episode
     @Override
-    public void read() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'read'");
+    public void update(Episode episode) 
+    {
+        Episode s = read(episode.getId());
+
+        if(s == null) return false;
+        if(!super.update(episode)) return false; 
+        
+        if(!s.getName().equals(episode.getName()))
+        {
+            nameIndex.delete(new EpisodeNameIdPair(s.getName(), s.getId()));
+            nameIndex.create(new EpisodeNameIdPair(episode.getName(), episode.getId()));
+        }
+
+        return true;
     }
 
-    // Update Episode
-    @Override
-    public void update(Episode newEpisode) {
-        Episode e = read(newEpisode.getId());
-        if(super.update(newEpisode)) {
-            if(newEpisode.getId().compareTo(e.getId()!=0)) {
-                indirectId.()
-            }
+    public Episode[] read(String name) throws Exception
+    {
+        if(name.isEmpty()) return null;
+
+        ArrayList<EpisodeNameIdPair> pairs = nameIndex.read(new EpisodeNameIdPair(name, -1));
+        if(pairs.size() == 0) return null;
+
+        Episode[] episode = new Episode[pairs.size()];
+        
+        for(int i = 0; i < pairs.size(); i++)
+        {
+            episode[i] = read(pairs.getId());
         }
     }
-    // Delete Episode
+
     @Override
-    public void delete(int id) throws Exception{
-        Episode e = super.read(id);
-        if(e != null) {
-            if(super.delete(id)) {
-                return indirectIndexId.delete(ParNameId.hash(e.getName));
-            }
+    public boolean delete(int id) throws Exception 
+    {
+        Episode episode = read(id);
+        
+        if(episode == null) return false;
+        if(!super.delete(id)) return false;
+        return nameIndex.delete(new EpisodeNameIdPair(episode.getName(), id));
+    }
+
+    public boolean delete(String name) throws Exception
+    {
+        if(name.isEmpty())
+            return false;
+
+        ArrayList<EpisodeNameIdPair> pairs = nameIndex.read(new EpisodeNameIdPair(name, -1));
+        for(EpisodeNameIdPair pair : pairs)
+        {
+            if(pair.getName().equals(name))
+                return delete(pair.getId);
         }
+
         return false;
     }
 }
